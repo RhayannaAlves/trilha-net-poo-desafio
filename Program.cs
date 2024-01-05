7901d0891d5d22d25b210ebeca415c8be34b5779
Rhayanna Alves <rhayanna.paz@gmail.com>
2024-01-05 10:59:20 -0300

    Desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//Implementado
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456789", modelo: "3310", imei: "111111111111", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "15 PRO", imei: "000000000000", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
